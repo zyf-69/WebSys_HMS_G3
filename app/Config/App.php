da223b40f5dea0636777d0a914938b3f5298e652
zyf-69 <ztndn851@gmail.com>
2025-12-15 01:17:36 +0800

    security: Remove /public from URLs for better security
    - Update baseURL to remove /public/ path
    - Add root .htaccess to redirect /public/ URLs
    - Configure URL rewriting to hide public folder
    - Add security rules to deny access to sensitive directories
    - Improve system security by hiding internal structure

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/WebSys_HMS_G3/public/';
+    public string $baseURL = 'http://localhost/WebSys_HMS_G3/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
